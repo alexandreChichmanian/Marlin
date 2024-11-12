14c545bc3c0ddedaa008f50784ccbb10a5229967
alexandreChichmanian <alexandre.chichmanian@gmail.com>
2024-11-12 00:12:31 -0500

    Update Configuration.h
    reduce mm/s too high on z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1288,7 +1288,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.74, 78.74, 2560, 105 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.74, 78.74, 400, 105 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
